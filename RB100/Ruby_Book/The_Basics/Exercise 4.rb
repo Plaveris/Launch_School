779ba5dc25c72f75d12215f95062e85bb1bbfeb4
Ryan Bloomfield <bloomfield992@gmail.com>
2022-07-09 15:38:38 -0700

    Exercise 4!

---
+++ RB100/Ruby_Book/The_Basics/Exercise 4.rb
@@ -0,0 +1,13 @@
+=begin
+Use the dates from the previous example and store them in an array. Then make your program output the same thing as
+exercise 3.
+=end 
+
+#Creates an array called "dates" with dates from Exercise 3
+dates = [1975, 2004, 2013, 2001, 1981]
+
+puts dates[0] #Prints 1975
+puts dates[1] #Prints 2004
+puts dates[2] #Prints 2013
+puts dates[3] #Prints 2001
+puts dates[4] #Prints 1981
