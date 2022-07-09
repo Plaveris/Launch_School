42f182b58d356549d4946a5ed08753e2b22c77a4
Ryan Bloomfield <bloomfield992@gmail.com>
2022-07-09 15:58:41 -0700

    Exercise 6
    The squares of 3 Floats

---
+++ RB100/Ruby_Book/The_Basics/Exercises/Exercise 6.rb
@@ -0,0 +1,5 @@
+#Write a program that calculates the squares of 3 float numbers of your choosing and outputs the result to the screen.
+
+puts 1.5 * 1.5 #Prints 2.25
+puts 3.5 * 3.5 #Prints 12.25
+puts 5.5 * 5.5 #Prints 30.25
