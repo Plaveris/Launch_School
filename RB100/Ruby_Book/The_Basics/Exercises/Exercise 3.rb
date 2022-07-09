f62256cd4cefd47c3c9cd22be106011b1b5af26f
Ryan Bloomfield <bloomfield992@gmail.com>
2022-07-09 15:26:26 -0700

    Exercise 3

---
+++ RB100/Ruby_Book/The_Basics/Exercises/Exercise 3.rb
@@ -0,0 +1,19 @@
+=begin 
+Write a program that uses a hash to store a list of movie titles with the year they came out. 
+Then use the puts command to make your program print out the year of each movie to the screen. 
+The output for your program should look something like this.
+=end 
+
+
+#Createx a hash named "movies" and setting their values
+movies = { :jaws => 1975, 
+           :troy => 2004, 
+           :promethus => 2013, 
+           :bruce_almighty => 2001, 
+           :animal_house => 1981 }
+
+puts movies[:jaws] #prints 1975
+puts movies[:troy] #prints 2004
+puts movies[:promethus] #prints 2013
+puts movies[:bruce_almighty] #prints 2001
+puts movies[:animal_house] #prints 1981
