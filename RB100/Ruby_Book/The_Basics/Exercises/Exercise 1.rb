daf51a210ee613c3dabdf18a7be5db2842d22916
Ryan Bloomfield <bloomfield992@gmail.com>
2022-07-09 14:50:16 -0700

    Exercise 1
    My first Ruby Exercise!

---
+++ RB100/Ruby_Book/The_Basics/Exercises/Exercise 1.rb
@@ -0,0 +1,6 @@
+# Add two strings together that, when concatenated, return your first and last name as your full name in one string.
+
+first_name = "Ryan"
+last_name = "Bloomfield"
+
+puts "#{first_name} #{last_name}" #Using String interpolation to print "Ryan Bloomfield" with a space versus "RyanBloomfield"
