19ac66fbb324c3c3a08d615e4a9e9c80885e1481
Ryan Bloomfield <bloomfield992@gmail.com>
2022-07-09 15:48:42 -0700

    Exercise 5

---
+++ RB100/Ruby_Book/The_Basics/Exercises/Exercise 5.rb
@@ -0,0 +1,6 @@
+#Write a program that outputs the factorial of the numbers 5, 6, 7, and 8.
+
+puts 5 * 4 * 3 * 2 * 1 #Prints 120, the factorial of 5
+puts 6 * 5 * 4 * 3 * 2 * 1 #Prints 720, the factorial of 6
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1 #Prints 5040, the factorial of 7
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1 #Prints 40320, the factorial of 8
