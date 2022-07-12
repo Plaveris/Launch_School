8a61cf38f94884d5275cf43a4cd7759d84905391
Ryan Bloomfield <bloomfield992@gmail.com>
2022-07-12 15:01:45 -0700

    Playing around with name.rb and age.rb
    Adding on from Exercise 1 - This is exercise 3

--- RB100/Ruby_Book/Variables/Exercises/name.rb
+++ RB100/Ruby_Book/Variables/Exercises/name.rb
@@ -8,3 +8,17 @@ puts "What is your name?"
 name = gets.chomp 
 
 puts "Nice to meet you, #{name}!"
+
+=begin 
+Add another section onto name.rb that prints the name of the user 10 times. You must do this without explicitly writing
+   the puts method 10 times in a row. Hint: you can use the times method to do something repeatedly.
+=end 
+10.times { puts name } #Practicing one liners instead of using do/end 
+
+=begin
+This is the other form of notation, using do/end:
+
+  10.times do 
+    puts name 
+  end 
+=end
