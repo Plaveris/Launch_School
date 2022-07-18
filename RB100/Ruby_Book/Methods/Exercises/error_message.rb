0c164de2d8e1e8ecb782f773bc036163fff9b691
Ryan Bloomfield <bloomfield992@gmail.com>
2022-07-18 12:10:32 -0700

    edits

---
+++ RB100/Ruby_Book/Methods/Exercises/error_message.rb
@@ -0,0 +1,10 @@
+# Exercise 6: What does the following error message tell you?
+
+=begin 
+  ArgumentError: wrong number of arguments (1 for 2)
+  from (irb):1:in `calculate_product'
+  from (irb):4
+  from /Users/username/.rvm/rubies/ruby-2.5.3/bin/irb:12:in `<main>'
+=end 
+
+#You are calling a method named "calculate_product" that requires two arguments, but you are only providing one. 
