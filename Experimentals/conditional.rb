8f2999381bf9667f3a5fadd097cff0d150bf9916
Ryan Bloomfield <bloomfield992@gmail.com>
2022-07-18 12:10:21 -0700

    conditional.rb

---
+++ Experimentals/conditional.rb
@@ -0,0 +1,12 @@
+puts "Put in a number"
+a = gets.chomp.to_i
+
+if a == 3 
+  puts "a is 3"
+elsif a == 4
+  puts "a is 4"
+else
+  puts "a is neither 3, nor 4"
+end 
+
+# Depending on what the user puts in, they will get the output: a is 3, a is 4, or a is neither 3, nor 4  
