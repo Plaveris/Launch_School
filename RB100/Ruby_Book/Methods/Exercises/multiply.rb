e6985219838dc0bbc876412753d8c080681c3974
Ryan Bloomfield <bloomfield992@gmail.com>
2022-07-18 11:41:13 -0700

    multiply.rb

---
+++ RB100/Ruby_Book/Methods/Exercises/multiply.rb
@@ -0,0 +1,10 @@
+=begin 
+Write a program that includes a method called multiply that takes two arguments and returns the product of the 
+two numbers.
+=end 
+
+def multiply(num_1, num_2)
+  num_1 * num_2
+end 
+
+puts multiply(4,4)
